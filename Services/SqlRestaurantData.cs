2071ab46df238de24f8e6bd3aefc5d933deef701
Ghanie <abdallahabdulghany@gmail.com>
2020-11-18 23:32:41 +0200

    Setup Entity Framework, Initialize Database

---
+++ Services/SqlRestaurantData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OdeToFood.Data;
+using OdeToFood.Models;
+
+namespace OdeToFood.Services
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private readonly OdeToFoodDbContext _context;
+
+        public SqlRestaurantData(OdeToFoodDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            // If you have more than 10K Restaurants Names you should use IQueryable 
+            return _context.Restaurants;
+        }
+
+        public Restaurant Get(int id)
+        {
+            return _context.Restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public Restaurant Add(Restaurant restaurant)
+        {
+            _context.Add(restaurant);
+            
+            // You can save changes later in big projects as a method called Commit or whatever
+            _context.SaveChanges();
+            return restaurant;
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -4,7 +4,11 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using OdeToFood.Data;
 using OdeToFood.Services;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.Extensions.Configuration;
 
 
 namespace OdeToFood
@@ -13,11 +17,21 @@ namespace OdeToFood
 
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
+            // Add Db Service to Configurations and pass connectionString through GetConnectionString property in _configuration
+            services.AddDbContext<OdeToFoodDbContext>(
+                options=>options.UseSqlServer(_configuration.GetConnectionString("OdeToFood")));
+
+            services.AddScoped<IRestaurantData, SqlRestaurantData>();
             services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
